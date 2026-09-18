e20f74d4e210e7dea43c3c99dcf717bca7573516
kohyunmin <kodon0725@gmail.com>
2026-09-18 12:14:59 +0900

    3주차_점프맵게임_2025280071_고현민

--- unity_gg/Assets/playercontroler.cs
+++ unity_gg/Assets/playercontroler.cs
@@ -1,18 +1,56 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-
+ 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    private Vector2 moveInput;
+public string playerName = "Player";
+  public int hp = 100;
 
-    void OnMove(InputValue value)
-    {
-        moveInput = value.Get<Vector2>();
-    }
+  public float moveSpeed = 5f;
+public float jumpPower = 8f;
+private Rigidbody2D rb;
 
-    void Update()
-    {
-        transform.Translate(Vector3.right * moveInput.x * moveSpeed * Time.deltaTime);
-    }
+  private Vector2 moveInput;public Transform visual;
+
+void Start()
+{
+    rb = GetComponent<Rigidbody2D>();
+    Debug.Log(playerName + " 시작. 체력 " + hp);
+    Debug.Log("피격 후 체력 " + (hp - 30));
+    Debug.Log("달리기 속도 " + moveSpeed * 2);
+    Debug.Log("10 / 4 = " + 10 / 4);
+
+}
+  // 아래 OnMove, Update는 그대로
+
+ 
+ void OnMove(InputValue value)
+  {
+    moveInput = value.Get<Vector2>();
+    if (moveInput.x > 0)
+    {
+      visual.localScale = new Vector3(1, 1, 1);
+    }
+    else if (moveInput.x < 0)
+    {
+      visual.localScale = new Vector3(-1, 1, 1);
+    }
+  }
+
+ 
+ void Update()
+  {
+    Debug.Log("Update");
+    transform.Translate(Vector3.right * moveInput.x * moveSpeed * Time.deltaTime);
+  }
+void OnJump(InputValue value)
+  {
+    if (value.isPressed)
+    {
+      Debug.Log("점프!");
+      rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
+    }
+  }
+
+
 }
